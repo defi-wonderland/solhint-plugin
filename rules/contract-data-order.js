7138db322676e20ee9243a1bb233a9f25de87696
0xRaccoon <raccoon@defi.sucks>
2023-02-07 17:45:29 -0300

    fix: contract data order rule
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- rules/contract-data-order.js
+++ rules/contract-data-order.js
@@ -3,10 +3,12 @@ const BaseChecker = require('../base-checker')
 const ruleId = 'contract-data-order'
 const meta = {
   type: 'Best Practices',
+
   docs: {
     description: 'Enforce the specified ordering of data in contracts',
-    category: 'Best Practices',
+        category: 'Best Practices',
   },
+
   isDefault: true,
   recommended: true,
   defaultSetup: 'warn',
@@ -22,12 +24,15 @@ class ContractDataOrderChecker extends BaseChecker {
     if (node.kind !== 'contract') return;
 
     const contractMembers = node.subNodes;
+    const unOrderedMembers = [];
     const constants = [];
     const immutableVariables = [];
     const stateVariables = [];
     
     contractMembers.forEach(member => {
+
       if (member.type === 'StateVariableDeclaration') {
+        unOrderedMembers.push(member);
         member.variables.forEach((variable) => {
           if (variable.isDeclaredConst) {
             constants.push(member);
@@ -41,8 +46,8 @@ class ContractDataOrderChecker extends BaseChecker {
     });
     
     const orderedMembers = [...constants, ...immutableVariables, ...stateVariables];
-    for (let i = 0; i < contractMembers.length; i++) {
-      if (contractMembers[i] !== orderedMembers[i]) {
+    for (let i = 0; i < orderedMembers.length; i++) {
+      if (unOrderedMembers[i] !== orderedMembers[i]) {
         this.error(
           node,
           `The order of data in contract ${node.name} should be: Constants, Immutable variables, State Variables`
