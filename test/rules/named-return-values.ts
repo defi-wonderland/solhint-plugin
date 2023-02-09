a032c8cc2bca1c35f30c680d0a577cd4572188b1
0xRaccoon <raccoon@defi.sucks>
2023-02-09 18:45:24 -0300

    applied pr requested changes

--- test/rules/named-return-values.ts
+++ test/rules/named-return-values.ts
@@ -1,13 +1,15 @@
 import assert from 'assert';
-import linter from '../index';
+import linter from '../utils/index';
 const contractWith = require('solhint/test/common/contract-builder').contractWith;
+
+const config = {
+  rules: { 'named-return-values': 'error' },
+};
 
 describe('Linter - named-return-values', () => {
   it('should raise error for unnamed return values', () => {
     const code = contractWith('function test() returns (uint) {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 1);
     assert.ok(report.messages[0].message == `Return value 'uint' in function 'test' must be named`);
@@ -15,9 +17,7 @@ describe('Linter - named-return-values', () => {
 
   it('should raise error for unnamed return values in multiple return values', () => {
     const code = contractWith('function test() returns (uint, int) {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
     assert.equal(report.errorCount, 2);
     assert.ok(report.messages[0].message == `Return value 'uint' in function 'test' must be named`);
     assert.ok(report.messages[1].message == `Return value 'int' in function 'test' must be named`);
@@ -25,9 +25,7 @@ describe('Linter - named-return-values', () => {
 
   it('should raise error for unnamed return value and named return value', () => {
     const code = contractWith('function test() returns (uint, uint _test) {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 1);
     assert.ok(report.messages[0].message == `Return value 'uint' in function 'test' must be named`);
@@ -35,27 +33,21 @@ describe('Linter - named-return-values', () => {
 
   it('should not raise error for named return values', () => {
     const code = contractWith('function test() returns (uint a) {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 0);
   });
 
   it('should not raise error for multiple named return values', () => {
     const code = contractWith('function test() returns (uint a, uint b) {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 0);
   });
 
   it('should not raise error for no return values', () => {
     const code = contractWith('function test() {}');
-    const report = linter.processStr(code, {
-      rules: { 'named-return-values': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 0);
   });
--- test/rules/non-state-vars-leading-underscore.ts
+++ test/rules/non-state-vars-leading-underscore.ts
@@ -1,23 +1,22 @@
 import assert from 'assert';
-import linter from '../index';
+import linter from '../utils/index';
 const contractWith = require('solhint/test/common/contract-builder').contractWith;
+
+const config = {
+  rules: { 'non-state-vars-leading-underscore': 'error' },
+};
 
 describe('non-state-vars-leading-underscore', () => {
   it('should raise an error if a block variable does not start with an underscore', () => {
     const code = contractWith('function foo() public { uint myVar; }');
-    const report = linter.processStr(code, {
-      rules: { 'non-state-vars-leading-underscore': 'error' },
-    });
-    console.log(report.messages[0].message);
+    const report = linter.processStr(code, config);
     assert.equal(report.errorCount, 1);
     assert.ok(report.messages[0].message == `'myVar' should start with _`);
   });
 
   it('should not raise an error if a block variable starts with an underscore', () => {
     const code = contractWith('function foo() public { uint _myVar; }');
-    const report = linter.processStr(code, {
-      rules: { 'non-state-vars-leading-underscore': 'error' },
-    });
+    const report = linter.processStr(code, config);
 
     assert.equal(report.errorCount, 0);
   });
