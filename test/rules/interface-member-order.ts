3f2ea1f646dd2e7038cf9afcbe288b75a53873aa
0xRaccoon <raccoon@defi.sucks>
2023-02-10 18:22:34 -0300

    fix: applied suggested pr comments
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- test/rules/interface-member-order.ts
+++ test/rules/interface-member-order.ts
@@ -6,33 +6,35 @@ const config = {
     rules: { 'interface-member-order': 'error' },
 };
 
+const interfaceName = 'ITest';
+
 describe('Linter - interface-member-order', () => {
     it('should raise error when errors first than events', () => {
-        const code = interfaceWith('ITest', 'error TestError(); event TestEvent();');
+        const code = interfaceWith(interfaceName, 'error TestError(); event TestEvent();');
         const report = linter.processStr(code, config);
 
         assert.equal(report.errorCount, 1);
-        assert.ok(report.messages[0].message == `The order of members in the interface ITest interfaces should be: Events, Errors, Structs, Functions`);
+        assert.ok(report.messages[0].message == `The order of members in the interface ${interfaceName} interfaces should be: Events, Errors, Structs, Functions`);
     });
 
     it('should raise error when structs first than errors', () => {
-        const code = interfaceWith('ITest', 'struct TestStruct { uint256 data; } error Error();');
+        const code = interfaceWith(interfaceName, 'struct TestStruct { uint256 data; } error Error();');
         const report = linter.processStr(code, config);
 
         assert.equal(report.errorCount, 1);
-        assert.ok(report.messages[0].message == `The order of members in the interface ITest interfaces should be: Events, Errors, Structs, Functions`);
+        assert.ok(report.messages[0].message == `The order of members in the interface ${interfaceName} interfaces should be: Events, Errors, Structs, Functions`);
     });
 
     it('should raise error when functions first than structs', () => {
-        const code = interfaceWith('ITest', 'function testFunction(){} struct TestStruct { uint256 data; }');
+        const code = interfaceWith(interfaceName, 'function testFunction(){} struct TestStruct { uint256 data; }');
         const report = linter.processStr(code, config);
 
         assert.equal(report.errorCount, 1);
-        assert.ok(report.messages[0].message == `The order of members in the interface ITest interfaces should be: Events, Errors, Structs, Functions`);
+        assert.ok(report.messages[0].message == `The order of members in the interface ${interfaceName} interfaces should be: Events, Errors, Structs, Functions`);
     });
 
     it('should not raise error for interface member order events, errors, structs and functions', () => {
-        const code = interfaceWith('ITest', 'event TestEvent(); error TestError(); struct TestStruct { uint256 data; } function testFunction(){}');
+        const code = interfaceWith(interfaceName, 'event TestEvent(); error TestError(); struct TestStruct { uint256 data; } function testFunction(){}');
         const report = linter.processStr(code, config);
 
         assert.equal(report.errorCount, 0);
